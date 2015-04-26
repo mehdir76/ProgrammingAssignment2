63f8ec44828b7896d74eb0c90b6c19d8131fa43b
mehdir76 <mehdir76@users.noreply.github.com>
2015-04-26 18:45:26 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,11 +13,13 @@ makeCacheMatrix <- function(x = matrix()) {
              ## Initialize cache_mtr to NULL so we can tell when cacheSolve has run at least once.           
              cache_mtr <<- NULL                                      
          } 
-         ## Create function to get/return the matrix passed in the command line call to '$set 
+           ## Create function to get/return the matrix passed in the command line call to '$set 
            get <- function() cache_x 
-         ## Create function to set the value of cache_mtr in cache to the value of local_mtr passed in the call to '$set_cache_mtr. 
+           ## Create function to set the value of cache_mtr in cache to the value of local_mtr passed 
+           ## in the call to '$set_cache_mtr. 
            set_cache_mtr <- function(local_mtr) cache_mtr <<- local_mtr            
-         ## Create function to retrieve value of cache_mtr from cache and return cache_mtr to the caller so we can check it for NULL 
+           ## Create function to retrieve value of cache_mtr from cache and return cache_mtr to the caller
+           ## so we can check it for NULL 
            get_cache_mtr <- function() cache_mtr                       
            list(set = set, get = get,
                 set_cache_mtr = set_cache_mtr,
@@ -32,12 +34,12 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve then returns the last_matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        ## Get the value for mtr in the cache environment and put it in a local_mtr. 
+         ## Return a matrix that is the inverse of 'x'
+         ## Get the value for mtr in the cache environment and put it in a local_mtr. 
          local_mtr<- x$get_cache_mtr()  
          ## Check to see if mtr is NULL.
          if(!is.null(local_mtr)) { 
-           ## If mtr is not NULL, return the value of mtr with a message.
+         ## If mtr is not NULL, return the value of mtr with a message.
                  message("Retrieving cached matrix...")   
                    return(local_mtr) 
          }         ## If we get here, mtr was NULL 
@@ -45,7 +47,8 @@ cacheSolve <- function(x, ...) {
          first_matrix <- x$get()   
          ## Use solve() to invert the first_matrix, then save it in last_matrix. 
          last_matrix <- solve(first_matrix)  
-         ## Call x$set_cache_m() in makeCacheMatrix to set mtr in the cache environment to the local non-NULL inverted result in last_matrix 
+         ## Call x$set_cache_mtr() in makeCacheMatrix to set mtr in the cache environment 
+         ## to the local non-NULL inverted result in last_matrix 
          x$set_cache_mtr(last_matrix)   
          ## Return last_matrix if cache_mtr is not null.  
          last_matrix   
